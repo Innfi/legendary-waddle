c99539348ee53dc55e6892020f60524920191ea7
innfi <innfi.world@gmail.com>
2025-03-19 22:31:05 +0900

    backend: slowly remembering how to walk...

--- backend/web-server/src/main.rs
+++ backend/web-server/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::dev::Server;
+use actix_web::{App, HttpServer, web};
+use std::net::TcpListener;
+
+use web_server::routes::health_check;
+
+fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+  let server =
+    HttpServer::new(move || App::new().route("/health_check", web::get().to(health_check)))
+      .listen(listener)?
+      .run();
+
+  Ok(server)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+  println!("Hello, world!");
+
+  let listener = TcpListener::bind("localhost:8080")?;
+
+  run(listener)?.await
 }
