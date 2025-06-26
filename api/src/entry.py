10a441469d18ac8bd36a2341fe130d057f8fa6a9
innfi <innfi.world@gmail.com>
2025-06-26 22:37:48 +0900

    api: setting up dictionary

--- api/src/entry.py
+++ api/src/entry.py
@@ -1,18 +1,42 @@
 from workers import Response
-from urllib.parse import urlparse
- 
+from urllib.parse import urlparse, urlsplit, parse_qsl
+
+def get_users(request, path, method):
+  query = urlsplit(request.url).query
+  query_params = dict(parse_qsl(query))
+
+  return Response(f"{method} {path}: {query_params}")
+
+def post_users(request, path, method):
+  return Response(f"{method} {path}")
+
+def get_products(request, path, method):
+  return Response(f"{method} {path}")
+
+def patch_products(request, path, method):
+  return Response(f"{method} {path}")
+
+routes = {
+  '/users': {
+    'GET': get_users,
+    'POST': post_users,
+  },
+  '/products': {
+    'GET': get_products,
+    'PATCH': patch_products,
+  },
+}
+
 async def on_fetch(request, env):
   method = request.method
-
   url = urlparse(request.url)
   path = url.path
 
-  if path == "/api/users":
-    return Response(method + "/api/users")
-  elif path == "/api/products":
-    return Response(method + "/api/products")
+  handler = routes.get(path, {}).get(method)
+  if handler:
+    return handler(request, path, method)
   else:
-    return Response(method + "not found", status=404)
+    return Response("not found", status=404)
 
   # name = await env.spinach_kv.get("name")
   # if name is None:
