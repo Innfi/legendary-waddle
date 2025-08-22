3b581407d2c1b5c9cd1291920ab9231dce5a6b29
innfi <innfi.world@gmail.com>
2025-08-23 01:31:58 +0900

    fix error

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // const env = loadEnv(mode, process.cwd(), '');
   console.log(`env: ${process.env.VITE_BACKEND_URL}`);
 
