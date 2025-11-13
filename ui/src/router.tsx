a51559089d41f21aad2ef4742264a6852cd04c31
innfi <innfi.world@gmail.com>
2025-11-13 22:00:28 +0900

    ui: minimalizing

--- ui/src/router.tsx
+++ ui/src/router.tsx
@@ -4,9 +4,6 @@ import { PrivatePage } from "./components/auth/private.page";
 import LandingPage from "./pages/LandingPage";
 import DashboardPage from "./pages/DashboardPage";
 import WorkoutPage from "./pages/WorkoutPage";
-import SchedulePage from "./pages/SchedulePage";
-import ProfilePage from "./pages/ProfilePage";
-import WorkoutHistoryPage from "./pages/WorkoutHistoryPage";
 
 const routes: RouteObject[] = [
   { path: '/', element: <LandingPage /> },
@@ -26,30 +23,6 @@ const routes: RouteObject[] = [
       </PrivatePage>
     ),
   },
-  {
-    path: '/schedule',
-    element: (
-      <PrivatePage>
-        <SchedulePage />
-      </PrivatePage>
-    ),
-  },
-  {
-    path: '/profile',
-    element: (
-      <PrivatePage>
-        <ProfilePage />
-      </PrivatePage>
-    ),
-  },
-  {
-    path: '/history',
-    element: (
-      <PrivatePage>
-        <WorkoutHistoryPage />
-      </PrivatePage>
-    ),
-  }
 ];
 
 export function RenderRouter() {
